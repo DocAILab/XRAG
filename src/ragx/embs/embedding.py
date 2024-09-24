e3371045eec066872bbe1fc9637839d2a41d6f36
YiFei <932525787@qq.com>
2024-09-25 00:11:07 +0800

    fix gpt4o

--- src/ragx/embs/embedding.py
+++ src/ragx/embs/embedding.py
@@ -1,6 +1,5 @@
-# from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.legacy.embeddings import HuggingFaceEmbedding
-
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+# from llama_index.legacy.embeddings import HuggingFaceEmbedding
 
 def get_embedding(name):
     return HuggingFaceEmbedding(
