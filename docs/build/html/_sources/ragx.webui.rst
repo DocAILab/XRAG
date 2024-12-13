88741850e04d7a6c7807a605461e624c6c0a98b3
hzt <3061613175@qq.com>
2024-12-13 22:48:18 +0800

    update docs for proper config with sys.path && update gitignore for docs/

---
+++ docs/build/html/_sources/ragx.webui.rst
@@ -0,0 +1,29 @@
+ragx.webui package
+==================
+
+Submodules
+----------
+
+ragx.webui.app module
+---------------------
+
+.. automodule:: ragx.webui.app
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+ragx.webui.run module
+---------------------
+
+.. automodule:: ragx.webui.run
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: ragx.webui
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,25 +11,41 @@ copyright = '2024, HZT'
 author = 'HZT'
 release = '1.0'
 
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../../src')) 
+print(sys.path)
+
+# 手动 Mock Config 类或提供 config.toml 路径
+from ragx.config import Config
+
+config_path = os.path.abspath('../../config.toml')
+Config(config_file_path=config_path)
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',  # 用于支持 Google 和 NumPy 风格的 docstring
-    'sphinx.ext.viewcode',  # 用于添加源代码链接
+    'sphinx.ext.autodoc',        # 自动生成文档
+    'sphinx.ext.viewcode',       # 添加 "view source" 链接
+    'sphinx.ext.napoleon',       # 支持 Google 和 NumPy 风格 docstring
+    'sphinx_autodoc_typehints',  # 支持 Python 类型提示
 ]
+
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-root_doc = 'index'
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
 
 html_static_path = ['_static']
+# autodoc_mock_imports = ['ragx.config', 'ragx.llms']
+
